1d7bd0236dbc1ad289f709eaeae677c5b7d02229
DinhVuTienSon <st1451151@gmail.com>
2023-09-18 23:34:15 +0700

    create notification layout

---
+++ app/src/main/java/vn/edu/usth/facebook/NotificationAdapter.java
@@ -0,0 +1,59 @@
+package vn.edu.usth.facebook;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import vn.edu.usth.facebook.model.Friends;
+import vn.edu.usth.facebook.model.Notifications;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
+    private ArrayList<Notifications> notifications;
+    private NotificationFragment context;
+    public NotificationAdapter(ArrayList<Notifications> notifications, NotificationFragment context){
+        this.notifications = notifications;
+        this.context = context;
+    }
+    @NonNull
+    @Override
+    public NotificationAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notification_container, parent, false);
+        return new NotificationAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotificationAdapter.ViewHolder holder, int position) {
+        Notifications notification = notifications.get(position);
+
+        Picasso.get().load(notification.getNotification_ava()).into(holder.notification_ava);
+        holder.notification_content.setText(notification.getNotification_content());
+        holder.notification_time.setText(notification.getNotification_time());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notifications.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private CircleImageView notification_ava;
+        private TextView notification_content, notification_time;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            notification_ava = itemView.findViewById(R.id.notification_ava);
+            notification_content = itemView.findViewById(R.id.notification_content);
+            notification_time = itemView.findViewById(R.id.notification_time);
+        }
+    }
+}
---
+++ app/src/main/java/vn/edu/usth/facebook/model/Notifications.java
@@ -0,0 +1,37 @@
+package vn.edu.usth.facebook.model;
+
+public class Notifications {
+    private String notification_ava;
+    private String notification_content;
+    private String notification_time;
+
+    public String getNotification_ava() {
+        return notification_ava;
+    }
+
+    public void setNotification_ava(String notification_ava) {
+        this.notification_ava = notification_ava;
+    }
+
+    public String getNotification_content() {
+        return notification_content;
+    }
+
+    public void setNotification_content(String notification_content) {
+        this.notification_content = notification_content;
+    }
+
+    public String getNotification_time() {
+        return notification_time;
+    }
+
+    public void setNotification_time(String notification_time) {
+        this.notification_time = notification_time;
+    }
+
+    public Notifications(String notification_ava, String notification_content, String notification_time) {
+        this.notification_ava = notification_ava;
+        this.notification_content = notification_content;
+        this.notification_time = notification_time;
+    }
+}
