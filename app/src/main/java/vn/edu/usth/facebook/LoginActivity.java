5550b79428603421610193fcaa29ce117dadfeab
DinhVuTienSon <st1451151@gmail.com>
2023-09-21 21:05:08 +0700

    update login and register layout

---
+++ app/src/main/java/vn/edu/usth/facebook/LoginActivity.java
@@ -0,0 +1,67 @@
+package vn.edu.usth.facebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private boolean isLoggedIn = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login); // Set your activity's layout here
+
+        Button loginButton = findViewById(R.id.btnLogin); // Replace with your actual button ID
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Simulate a successful login
+                isLoggedIn = true;
+                handleLogin();
+            }
+        });
+        TextView register_textView = findViewById(R.id.txtSignUp);
+        register_textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void handleLogin() {
+        String defaultUsername = "admin";
+        String defaultPassword = "admin";
+
+        // Retrieve the entered username and password
+        EditText usernameEditText = findViewById(R.id.editUsername);
+        EditText passwordEditText = findViewById(R.id.editPassword);
+
+        String enteredUsername = usernameEditText.getText().toString();
+        String enteredPassword = passwordEditText.getText().toString();
+
+        if (enteredUsername.equals(defaultUsername) && enteredPassword.equals(defaultPassword)) {
+            // Username and password match the default ones, consider it a successful login
+            isLoggedIn = true;
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // Username and password do not match, show an error message or handle it accordingly
+            Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean userIsLoggedIn() {
+        // Check the login status here (e.g., check the isLoggedIn flag)
+        return isLoggedIn;
+    }
+}
---
+++ app/src/main/java/vn/edu/usth/facebook/RegisterActivity.java
@@ -0,0 +1,38 @@
+package vn.edu.usth.facebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button register_btn = findViewById(R.id.register_btn);
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        TextView login_textView = findViewById(R.id.login_textView);
+        login_textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
